fa948a4736a7c254070ff6dddb6cd857f2399031
byteful <info@byteful.me>
2023-05-23 22:34:39 -0500

    just find any connected server instead of limbo

--- gameman-velocity/src/main/java/com/bongbong/cobl/gameman/velocity/limbo/LimboJoinHandler.java
+++ gameman-velocity/src/main/java/com/bongbong/cobl/gameman/velocity/limbo/LimboJoinHandler.java
@@ -2,15 +2,23 @@ package com.bongbong.cobl.gameman.velocity.limbo;
 
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.connection.LoginEvent;
+import com.velocitypowered.api.event.player.PlayerChooseInitialServerEvent;
+import com.velocitypowered.api.proxy.ProxyServer;
 import lombok.RequiredArgsConstructor;
 import net.elytrium.limboapi.api.Limbo;
 
 @RequiredArgsConstructor
 public class LimboJoinHandler {
   private final Limbo limboWorld;
+  private final ProxyServer proxy;
 
   @Subscribe
   public void onJoin(LoginEvent event) {
     limboWorld.spawnPlayer(event.getPlayer(), new LimboSession());
+  }
+
+  @Subscribe
+  public void onServerFind(PlayerChooseInitialServerEvent event) {
+    proxy.getAllServers().stream().findAny().ifPresent(event::setInitialServer);
   }
 }
